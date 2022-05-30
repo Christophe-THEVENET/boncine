e1539bc414ac22289c4fb95db63daabb16e23dde
Christophe THEVENET <christophethevenet2.0@gmail.com>
2022-05-30 18:32:36 +0200

    positionnement cards

--- index.php
+++ index.php
@@ -3,39 +3,86 @@
 include_once './view/header.html'
 ?>
 
-  <main>
+<main>
 
-    <!--  ========================= BANNIERE ============================= -->
+  <!--  ========================= BANNIERE ============================= -->
 
-    <div class="banniere">
-      <h3 class="slogan">Découvrez et partagez des films !</h3>
-    </div>
-
-
-    <!--  ========================= CARDS ============================= -->
+  <div class="banniere">
+    <h3 class="slogan">Découvrez et partagez des films !</h3>
+  </div>
 
 
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-          content.</p>
-        <a href="#" class="btn btn-warning">Go somewhere</a>
-        <a href="#" class="btn btn-danger">Go somewhere</a>
+  <!--  ========================= CARDS ============================= -->
+
+  <section class="d-flex justify-content-center align-items-center w-100 mt-5 flex-wrap">
+
+    <article class="m-3 rounded">
+      <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+          <a href="#" class="btn btn-warning">Go somewhere</a>
+          <a href="#" class="btn btn-danger">Go somewhere</a>
+        </div>
       </div>
-    </div>
-
-
-  </main>
+    </article>
 
 
 
+    <article class="m-3 rounded">
+      <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+          <a href="#" class="btn btn-warning">Go somewhere</a>
+          <a href="#" class="btn btn-danger">Go somewhere</a>
+        </div>
+      </div>
+    </article>
+
+    <article class="m-3 rounded">
+      <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+          <a href="#" class="btn btn-warning">Go somewhere</a>
+          <a href="#" class="btn btn-danger">Go somewhere</a>
+        </div>
+      </div>
+    </article>
+
+    <article class="m-3 rounded">
+      <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+          <a href="#" class="btn btn-warning">Go somewhere</a>
+          <a href="#" class="btn btn-danger">Go somewhere</a>
+        </div>
+      </div>
+    </article>
+
+    
+  </section>
+
+
+</main>
 
 
 
-  <!-- JavaScript Bundle with Popper -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+
+
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 
 </html>
