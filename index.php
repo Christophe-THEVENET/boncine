c093b1e9598b506bb52495b464ceea6c6698cf7e
Christophe THEVENET <christophethevenet2.0@gmail.com>
2022-05-31 09:27:50 +0200

    style des cartes films

--- index.php
+++ index.php
@@ -17,60 +17,22 @@ include_once './view/header.html'
   <section class="d-flex justify-content-center align-items-center w-100 mt-5 flex-wrap">
 
     <article class="m-3 rounded">
-      <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+      <div class="card">
+        <img src="https://www.telerama.fr/sites/tr_master/files/ea70fd04-d87a-40ef-8fad-ba3cce3d94ea_2.jpg" class="card-img-top" alt="affiche film avatar">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
-          <a href="#" class="btn btn-warning">Go somewhere</a>
-          <a href="#" class="btn btn-danger">Go somewhere</a>
+          <h5 class="card-title">Avatar</h5>
+          <h6 class="card-subtitle mb-2">Science-Fiction</h6>
+          <p class="card-text">Malgré sa paralysie, Jake Sully, un ancien marine immobilisé dans un fauteuil roulant, est resté un combattant au plus profond de son être. Il est recruté pour se rendre à des années-lumière de la Terre, sur Pandora, où de puissants groupes industriels exploitent un minerai rarissime destiné à résoudre la crise énergétique sur Terre.</p>
+          <a href="#" class="btn btn-danger"><i class="fa-solid fa-trash" data-bs-toggle="tooltip" data-bs-placement="top" title="supprimer"></i></a>
+          <a href="#" class="btn btn-warning"><i class="fa-solid fa-pen-to-square" data-bs-toggle="tooltip" data-bs-placement="top" title="éditer">
+            </i></a>
         </div>
       </div>
     </article>
 
 
 
-    <article class="m-3 rounded">
-      <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
-          <a href="#" class="btn btn-warning">Go somewhere</a>
-          <a href="#" class="btn btn-danger">Go somewhere</a>
-        </div>
-      </div>
-    </article>
 
-    <article class="m-3 rounded">
-      <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
-          <a href="#" class="btn btn-warning">Go somewhere</a>
-          <a href="#" class="btn btn-danger">Go somewhere</a>
-        </div>
-      </div>
-    </article>
-
-    <article class="m-3 rounded">
-      <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
-          <a href="#" class="btn btn-warning">Go somewhere</a>
-          <a href="#" class="btn btn-danger">Go somewhere</a>
-        </div>
-      </div>
-    </article>
-
-    
   </section>
 
 
@@ -83,6 +45,7 @@ include_once './view/header.html'
 
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+<script src="./js/script.js"></script>
 </body>
 
 </html>
