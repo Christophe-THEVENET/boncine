c3e32dbb26ebb9a9ddab966d800b7ad2eb829269
Christophe THEVENET <christophethevenet2.0@gmail.com>
2022-11-01 06:22:25 +0100

    style du header + ajout liens connect

--- view/_header.php
+++ view/_header.php
@@ -8,7 +8,6 @@
   <!-- font awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- bootstrap CSS only -->
-  
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <!-- !!!!!!!!! pour pointer la feuille de style sur plusieurs fichiers php bien partir de localhost -->
   <link rel="stylesheet" href="/studi/exo_php/lives/Chris_CHEVALIER/allocine.com/styles/main.css">
@@ -16,29 +15,62 @@
 </head>
 
 <body>
-  <header>
-    <div class="header__title">
-      <h1>BonCiné.com</h1>
-    </div>
 
+  <!-- 
+  <nav class="navbar navbar-expand-lg flex-row-reverse">
+    <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="custom-toggler navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="/studi/exo_php/lives/Chris_CHEVALIER/allocine.com/index.php">Accueil</a>
+        </li>
+        <li class="nav-item ml-auto">
+          <a class="nav-link" href="/studi/exo_php/lives/Chris_CHEVALIER/allocine.com/View/createMovie.php">Ajouter un film</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ">
+        <li class="nav-item">
+          <a class="nav-link" href="/studi/exo_php/lives/Chris_CHEVALIER/allocine.com/index.php">Se connecter</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/studi/exo_php/lives/Chris_CHEVALIER/allocine.com/View/createMovie.php">S'inscrire</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+-->
     <!--  ========================= HEADER ============================= -->
 
-    <div class="header__nav d-flex justify-content-end">
-      <nav class="navbar navbar-expand-lg flex-row-reverse">
-        <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="custom-toggler navbar-toggler-icon"></span>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid">
+        <a class="navbar-brand header__title" href="#">BonCiné.com</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <!--     c est le me-auto qui pousse a droite   -->
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
               <a class="nav-link" href="/studi/exo_php/lives/Chris_CHEVALIER/allocine.com/index.php">Accueil</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="/studi/exo_php/lives/Chris_CHEVALIER/allocine.com/View/createMovie.php">Ajouter un film</a>
             </li>
+
+          </ul>
+          <ul class="navbar-nav mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="/studi/exo_php/lives/Chris_CHEVALIER/allocine.com/View/login.php">Se connecter</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/studi/exo_php/lives/Chris_CHEVALIER/allocine.com/View/register.php">S'inscrire</a>
+            </li>
           </ul>
         </div>
-      </nav>
-    </div>
-
+      </div>
+    </nav>
   </header>
+
